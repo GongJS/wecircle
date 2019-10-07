9533ffd302ac2e7e8d9337974f89966f9e0bbbce
GongJS <382663074@qq.com>
2019-10-07 21:17:32 +0800

    fix

--- app/src/pages/friend/Friend.tsx
+++ app/src/pages/friend/Friend.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import useReactRouter from 'use-react-router';
-import { Input, Button } from 'redell-ui'
+import { Input, Button,Loading } from 'redell-ui'
 import { post } from '../../utils/http'
 import { formateDate } from '../../utils/helper'
 import NavBar from '../../components/navbar/NavBar'
@@ -13,18 +13,19 @@ interface FriendProps extends React.HTMLAttributes<HTMLElement> {
 
 const Friend: React.FC<FriendProps> = () => {
   const [list, setList] = useState<any>([])
+  const [loading,setLoading] = useState(false)
   const { history } = useReactRouter()
   const [keyword, setKeyWord] = useState('')
-  const value = React.useRef('')
-  value.current = keyword
   const user = JSON.parse(localStorage.getItem('user') || '{}')
   const handleChange = (value: string) => {
     setKeyWord(value)
   }
   const handleSearch = async () => {
-    const res: any = await post('/api/user/search', { keyword: value.current, myId: user._id })
+    setLoading(true)
+    const res: any = await post('/api/user/search', { keyword, myId: user._id })
+    setLoading(false)
     if (res && res.code === 0) {
-      setList(res.data[0])
+        setList(res.data[0])
     }
   }
   const clearClick = () => {
@@ -47,8 +48,9 @@ const Friend: React.FC<FriendProps> = () => {
       <div className="search-bar">
         <Input onValueChange={handleChange} value={keyword} clearable addonAfter="查找" extraClick={handleSearch} clearClick={clearClick} placeholder="手机号/用户名" />
       </div>
+      <Loading name='loading' message="获取中..." loading={loading}/>
       {
-        list.friend && list.friend.map((item: any) => {
+        list && list.friend ? list.friend.map((item: any) => {
           return (
             <div className="item" key={item._id} onClick={() => goToPerson(item)}>
               <img src={item.avatar} alt="" className="avatar" />
@@ -58,10 +60,7 @@ const Friend: React.FC<FriendProps> = () => {
               <div className="time">{formateDate(item.create)}</div>
             </div>
           )
-        })
-      }
-      {
-        list.length === 0 ? <Empty title="暂无好友"></Empty> : null
+        }) : <Empty title="暂无好友"></Empty>
       }
       <div className="add-friend">
         <Button type="success" icon="roundadd" iconPosition="right" onClick={() => history.push('/add')}>
--- app/src/pages/friend/add/Add.tsx
+++ app/src/pages/friend/add/Add.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Input, Button, Modal,message } from 'redell-ui'
+import useReactRouter from 'use-react-router'
+import { Input,Loading } from 'redell-ui'
 import { post } from '../../../utils/http'
-import { formateDate } from '../../../utils/helper'
 import NavBar from '../../../components/navbar/NavBar'
 import Empty from '../../../components/empty/Empty'
 import './add.scss';
@@ -13,35 +13,27 @@ interface AddProps extends React.HTMLAttributes<HTMLElement> {
 
 const Add: React.FC<AddProps> = () => {
   const [list, setList] = useState([])
-  const [visible,setVisible] = useState(false)
+  const [loading,setLoading] = useState(false)
   const [keyword, setKeyWord] = useState('')
-  const [content, setContent] = useState('')
   const [showEmpty, setShowEmpty] = useState(false)
+  const { history } = useReactRouter()
   const user = JSON.parse(localStorage.getItem('user') || '{}')
-  let toUserId = React.useRef<string>()
   const handleKeyWordChange = (keyword: string) => {
     setKeyWord(keyword)
   }
-  const handleContentChange = (content:string)  => {
-    setContent(content)
-  }
   const handleSearch = async () => {
+    setLoading(true)
     const res: any = await post('/api/user/search', { keyword })
     setShowEmpty(true)
-    setList(res.data)
+    const filter = res.data.filter((v:any) => v._id !== user._id)
+    setList(filter)
+    setLoading(false)
   }
-  const showModal = async (id:string) => {
-    setVisible(true)
-    toUserId.current = id
-  }
-  const handleOk = async () => {
-    const res: any = await post('/api/request/addfriend', { content, myId: user._id,toUserId:toUserId.current })
-    if (res && res.code === 0) {
-      setVisible(false)
-      message.success('好友请求发送成功')
-    } else {
-      message.error(`${res.msg}`)
-    }
+  const goToPerson = (item:any) => {
+    history.push({
+      pathname: '/person',
+      state: { ...item }
+    })
   }
   return (
     <div className="add">
@@ -49,16 +41,15 @@ const Add: React.FC<AddProps> = () => {
       <div className="search-bar">
         <Input onValueChange={handleKeyWordChange} value={keyword} clearable addonAfter="查找" extraClick={handleSearch} placeholder="手机号/用户名"/>
       </div>
+      <Loading name='loading' message="获取中..." loading={loading}/>
       {
         list.map((item: any) => {
           return (
-            <div className="item" key={item._id} >
+            <div className="item" key={item._id} onClick={() => goToPerson(item)}>
               <img src={item.avatar} alt="" className="avatar" />
               <div className="right-content border-bottom">
                 <p className="nickname">{item.nickname}</p>
-                <Button type="success" onClick={() => showModal(item._id)} >添加</Button>
               </div>
-              <div className="time">{formateDate(item.create)}</div>
             </div>
           )
         })
@@ -66,10 +57,6 @@ const Add: React.FC<AddProps> = () => {
       {
         list.length === 0 && showEmpty? <Empty title="用户不存在"></Empty> : null
       }
-      <Modal visible={visible} onOk={handleOk}
-        onCancel={() => setVisible(false)} title="添加好友" className="wrapper">
-           <Input onValueChange={handleContentChange} value={content} clearable addonBefore="内容"/>
-      </Modal> 
     </div>
   );
 }
